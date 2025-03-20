8fb3d6976bcdda3831a3de2425a8722f0c30a12e
trungbnt <cr7204@gmail.com>
2025-03-20 12:50:23 +0700

    Update CORS configuration

--- server.js
+++ server.js
@@ -91,6 +91,17 @@ app.options('*', cors());
 // Middleware debug request
 app.use((req, res, next) => {
     // console.log(`${req.method} ${req.url} - Received at: ${new Date().toISOString()}`);
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
     next();
 });
 
